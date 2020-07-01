1cdcdee633cddad0b2653da3a9750992965f4b97
Jorge Barron <jorgeb8168711@gmail.com>
2020-06-30 22:25:45 -0500

    Fix action buttons

--- src/components/Viewer/Header/Header.jsx
+++ src/components/Viewer/Header/Header.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import Button from '../../Button/Button';
 import './Header.scss';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteEmail, moveToSpam, markAsUnread } from '../../../store/actions';
 
 const ViewerHeader = (props) => {
   const { emailId } = props;
   const dispatch = useDispatch();
+  const currentFilter = useSelector((state) => state.filterBy);
 
   return (
     <div className='ViewerHeader'>
       <div className='ViewerHeader-wrapper flex'>
-        <Button className='mr-2' color='warn' onClick={() => dispatch(deleteEmail(emailId))}>
-          Delete
-        </Button>
+        {currentFilter !== 'deleted' && (
+          <Button className='mr-2' color='warn' onClick={() => dispatch(deleteEmail(emailId))}>
+            Delete
+          </Button>
+        )}
 
-        <Button color='default' onClick={() => dispatch(moveToSpam(emailId))}>
-          Spam
-        </Button>
+        {currentFilter !== 'spam' && (
+          <Button color='default' onClick={() => dispatch(moveToSpam(emailId))}>
+            Spam
+          </Button>
+        )}
 
         <Button className='ml-auto' color='primary' onClick={() => dispatch(markAsUnread(emailId))}>
           Mark as unread
